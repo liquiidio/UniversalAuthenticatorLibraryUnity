5d2909e3fd9813ded69edd0a90fac837a0d2af60
corvinmadh <grizzlycorv@googlemail.com>
2022-12-16 14:08:31 +0100

    mini cleanup

--- Src/UiToolkit/Ui/ScreenBase.cs
+++ Src/UiToolkit/Ui/ScreenBase.cs
@@ -18,43 +18,21 @@ namespace UniversalAuthenticatorLibrary.Src.UiToolkit.Ui
             Hide();
         }
 
+        /// <summary>
+        /// Show this Screen (set it to visible)
+        /// </summary>
         public void Show()
         {
             Root.Show();
         }
 
+        /// <summary>
+        /// Hide this Screen (set it to invisible)
+        /// </summary>
+        /// <param name="element"></param>
         public void Hide()
         {
             Root.Hide();
-        }
-    }
-
-    public static class Utils
-    {
-        /// <summary>
-        /// Extension-method to show an UI Element (set it to visible)
-        /// </summary>
-        /// <param name="element"></param>
-        public static void Show(this VisualElement element)
-        {
-            if (element == null)
-                return;
-
-            element.style.visibility = Visibility.Visible;
-            element.style.display = DisplayStyle.Flex;
-        }
-
-        /// <summary>
-        /// Extension-method to hide an UI Element (set it to invisible)
-        /// </summary>
-        /// <param name="element"></param>
-        public static void Hide(this VisualElement element)
-        {
-            if (element == null)
-                return;
-
-            element.style.visibility = Visibility.Hidden;
-            element.style.display = DisplayStyle.None;
         }
     }
 }
